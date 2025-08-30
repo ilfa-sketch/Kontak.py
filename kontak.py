0470807dd2555c515f9d7fc4e4dd01830f463aa9
Faruq <141242297+ilfa-sketch@users.noreply.github.com>
2025-08-30 13:16:11 +0800

    Update kontak.py
    Change the contact storage to file.txt

--- kontak.py
+++ kontak.py
@@ -1,148 +1,219 @@
-kontak = [
-    {"Nama" : "budi",
-     "nomor": 1212,
-     "email": "budi@gmail.com"
-    },
-    {"Nama" : "aisah",
-     "nomor": 4545,
-     "email": "aisah@gmail.com"
-    },
-    {"Nama" : "fahran",
-     "nomor": 7676,
-     "email": "fahran@gmail.com"
-    },
-    {
-     "Nama" : "faruq",
-     "nomor": 8989,
-     "email": "faruq@gmail.com"
-    }]
-
-def tampilkan_kontak() :
-    for i, item in enumerate(kontak):
-        print(f"{i + 1}.Nama : {item["Nama"]}")
-        print(f"  Nomor : {item["nomor"]}")
-        print(f"  Email : {item["email"]}\n")
-
-def tambah_kontak() : 
-    index = int(len(kontak))
-    kontak_baru = {}
-    while True :
-        Nama_baru = input("Masukkan nama: ").strip()
-        if all(c.isalpha() or c.isspace() for c in Nama_baru) and Nama_baru:
-            print(f"Nama di buat : {Nama_baru}")
-            kontak_baru['Nama']= Nama_baru
-            break
-        elif Nama_baru.isdigit():
-            print("masukkan nama yang benar")
-            continue
-        else: 
-            print("masukkan nama yang benar")
-            continue
-    while True :
-        nomor_baru = input("masukkan nomor : ")
-        if nomor_baru.isdigit() :
-            print(f"nomor di tambahkan :{nomor_baru}")
-            kontak_baru['nomor'] = nomor_baru
-            break
-        elif nomor_baru.isalpha() :
-            print("no")
-            continue
-        else : 
-            print("no") 
-            continue
-    email_baru = input("masukkan email : ")
-    kontak_baru['email'] = email_baru + '@gmail.com'
-    kontak.append([])
-    kontak[index] = kontak_baru
-
-def hapus_kontak() :
-    while True :
-        input_user = input("kontak nomor berapa yang ingin anda hapus??: ")
-        if input_user.isdigit():
-            convert = int(input_user)
-            if convert <= len(kontak) and convert != 0:
-                kontak.pop(convert - 1)
+class Menu:
+    def __init__(self):
+        with open("kontak.txt",mode="r") as var:
+            self.content = [line.strip().split(',') for line in var.readlines()]
+    
+    def tampilkan_semua_kontak(self):
+        if len(self.content) == 0:
+            print("CONTACT IS EMPTY")
+            return 1
+        else:
+            for i,item in enumerate(self.content,start=1):
+                print(f"{i}.{item[1]} : {item[3]}")
+            return 2
+        
+        
+    def tampilkan_kontak_spesifik(self,input):
+        kontak = self.content[input-1]
+        print(f"Nama : {kontak[1]}\nEmail : {kontak[2]}\nNomor hp: {kontak[3]}")
+        
+    def edit_kontak(self):
+        while True:
+            inputUser = input("INGIN MENGEDIT NO BERAPA??\n>> ")
+            if inputUser.isdigit():
+                convert = int(inputUser)
+                if convert > 0 and convert <= len(self.content):
+                    self.tampilkan_kontak_spesifik(convert)
+                    self.pilihan_menu_edit_kontak(convert)
+                    break
+                else:
+                    print(f"MASUKKAN NOMOR 1 - {len(self.content)}")
+                    continue
+            else:
+                print("MASUKKAN INPUT NUMBER")
+                continue
+                
+    def pilihan_menu_edit_kontak(self,numb):
+        while True:
+            print("EDIT : \n1. NAMA\n2. EMAIL\n3. NOMOR")
+            inputUser = input(">> ")
+            if inputUser.isdigit():
+                convert = int(inputUser)
+                if convert > 0 and convert < 4:
+                    if inputUser == 1:
+                        self.ganti_nama(numb)
+                        break
+                    elif inputUser == 2:
+                        self.ganti_email(numb)
+                        break
+                    else:
+                        self.ganti_nomor(numb)
+                        break
+                else:
+                    print(f"MASUKKAN ANGKA SESUAI YG TERTERA")
+                    continue
+            else:
+                print("MASUKKAN INPUT NUMBER")
+                continue
+            
+    def ganti_nama(self,convert):
+        inputUser = input("NAMA BARU : ")
+        self.content[convert-1][1] = inputUser
+        Liaison_list = []
+        for item in self.content:
+            Liaison_list.append(",".join(item))
+        with open("kontak.txt",mode="w") as var:
+            for item in Liaison_list:
+                        var.write(item + "\n")
+                        
+                        
+    def ganti_gmail(self,convert):
+        inputUser = input("GMAIL BARU : ")
+        self.content[convert-1][2] = inputUser
+        Liaison_list = []
+        for item in self.content:
+            Liaison_list.append(",".join(item))
+        with open("kontak.txt",mode="w") as var:
+            for item in Liaison_list:
+                        var.write(item + "\n")
+                        
+                        
+    def ganti_nomor(self,convert):
+        inputUser = input("NOMOR BARU : ")
+        self.content[convert-1][3] = inputUser
+        Liaison_list = []
+        for item in self.content:
+            Liaison_list.append(",".join(item))
+        with open("kontak.txt",mode="w") as var:
+            for item in Liaison_list:
+                        var.write(item + "\n")
+                        
+                        
+    def tambah_kontak(self):
+        print("1. MENAMBAH KONTAK UMUM\n2. MENAMBAH KONTAK FAVORIT")
+        while True:
+            inputUser = input(">> ")
+            if inputUser.isdigit():
+                convert = int(inputUser)
+                if convert == 1:
+                    self.tambah_kontak_umum()
+                    break
+                elif convert == 2:
+                    self.tambah_kontak_fav()
+                    break
+                else:
+                    print("MASUKKAN ANGKA YG SESUAI")
+                    continue
+            else:
+                print("MASUKKAN INPUT YG BENAR")
+                continue
+            
+    def tambah_kontak_umum(self):
+        Liaison_list = []
+        Liaison_list.append("umum")
+        inputUser = input("NAMA : ")
+        Liaison_list.append(inputUser)
+        inputUser = input("GMAIL : ")
+        Liaison_list.append(inputUser)
+        while True:
+            inputUser = input("NOMOR : ")
+            if inputUser.isdigit():
+                Liaison_list.append(inputUser)
                 break
             else:
-                print("masukkan index atau angka yang benar")
+                print("MASUKKAN NOMOR YG BENAR")
                 continue
-        elif not input_user:
-            print("input kosong")
-            continue
-        else:
-            print("masukkan nomor yang benar")
-            continue
-            
-
-def ubah_nama(index):
-    while True :
-        input_nama = input("Nama baru >> ")
-        if all(c.isalpha() or c.isspace() for c in input_nama) and input_nama:
-            kontak[index-1]["Nama"] = input_nama
-            break
-        else :
-            print("masukkan index yamg benar!!!")
-            continue
-
-def ubah_nomor(index):
-    while True :
-        input_nomor = input("nomor baru >> ")
-        if input_nomor.isdigit():
-            convert = int(input_nomor)
-            kontak[index-1]["nomor"] = convert
-            break
-        else:
-            print("masukkan nomor yang benar!!")
-            continue
-
-def ubah_email(index):
-    input_email = input("Masukkan nama email baru >>") + "@gmail.com"
-    kontak[index-1]["email"] = input_email
-
-
-def edit_kontak() :
-    while True :
-        user_input = input("Kontak berapa yg ingin anda edit?? ")
-        convert = int(user_input)
-        if user_input.isdigit() and convert <= len(kontak) and convert != 0:
-            print("INGIN MENGUBAH APA??\n1. Nama\n2. Nomor\n3. Email")
-            input_user_change = int(input(">> "))
-            if input_user_change == 1:
-                ubah_nama(convert)
+        with open("kontak.txt",mode="a") as var:
+            item = ",".join(Liaison_list)
+            var.write(item+"\n")
+        print("TELAH DI TAMBAHKAN")
+        
+        
+    def tambah_kontak_fav(self):
+        changing_list = []
+        changing_list.append("favorit")
+        inputUser = input("NAMA : ")
+        changing_list.append(inputUser)
+        inputUser = input("GMAIL : ")
+        changing_list.append(inputUser)
+        while True:
+            inputUser = input("NOMOR : ")
+            if inputUser.isdigit():
+                changing_list.append(inputUser)
                 break
-            elif input_user_change == 2:
-                ubah_nomor(convert)
-                break
-            elif input_user_change == 3:
-                ubah_email(convert)
-                break
-            else :
-                print("masukkan angka yg sesuai")
+            else:
+                print("MASUKKAN NOMOR YG BENAR")
                 continue
-        else:
-            print("Masukkan angka atau index yang sesuai")
-            continue
+        self.content.insert(0,changing_list)
+        Liaison_list = []
+        for item in self.content:
+            Liaison_list.append(",".join(item))
+        with open("kontak.txt",mode="w") as var:
+            for item in Liaison_list:
+                        var.write(item + "\n")
+        print("TELAH DI TAMBAHKAN")
+        
+        
+    def hapus_kontak(self):
+        while True:
+            inputUser = input("MASUKKAN INDEX KONTAK NOMOR BERAPA YG INGIN DI HAPUS\n>> ")
+            if inputUser.isdigit():
+                convert = int(inputUser)
+                if convert > 0 and convert <= len(self.content):
+                    del self.content[convert-1]
+                    Liaison_list = []
+                    for item in self.content:
+                        Liaison_list.append(",".join(item))
+                    with open("kontak.txt",mode="w") as var:
+                        for item in Liaison_list:
+                            var.write(item + "\n")
+                    print("TELAH DI HAPUS")
+                    break
+                else:
+                    print("MASUKKAN ANGKA SESUAI INDEX")
+                    continue
+            else:
+                print("MASUKKAN ANGKA SESUAI INDEX")
+                continue
 
+
+
+
+menu = Menu()
 while True :
     print(f"\n\n{" "*5} KONTAK")
-    tampilkan_kontak()
-    print("PILIHAN :\n1. Tambahkan kontak\n2. Hapus kontak\n3. edit kontak\n4. keluar")
-    user_input = input(">> ")
-    convert = int(user_input)
-    if user_input.isdigit and convert <= 4 and convert != 0:
-        if convert == 1 :
-            tambah_kontak()
-            continue
-        elif convert == 2 :
-            hapus_kontak()
-            continue
-        elif convert == 3 :
-            edit_kontak()
-            continue
+    status = menu.tampilkan_semua_kontak()
+    if status == 1:
+        print("PILIHAN :\n1. Tambahkan kontak\n2. keluar")
+        user_input = input(">> ")
+        convert = int(user_input)
+        if user_input.isdigit and convert <= 2 and convert != 0:
+            if convert == 1 :
+                menu.tambah_kontak()
+                continue
+            else :
+                print("EXIT")
+                break
         else :
-            print("EXIT")
-            break
-    else :
-        print("Masukkan index yang benar")
-        continue
-        
+            print("Masukkan index yang benar")
+            continue
+    else:
+        print("PILIHAN :\n1. Tambahkan kontak\n2. Hapus kontak\n3. edit kontak\n4. keluar")
+        user_input = input(">> ")
+        convert = int(user_input)
+        if user_input.isdigit and convert <= 4 and convert != 0:
+            if convert == 1 :
+                menu.tambah_kontak()
+                continue
+            elif convert == 2 :
+                menu.hapus_kontak()
+                continue
+            elif convert == 3 :
+                menu.edit_kontak()
+                continue
+            else :
+                print("EXIT")
+                break
+        else :
+            print("Masukkan index yang benar")
+            continue
